374ac42bd6903a1047f95574803b59c95f5980f2
buggyCodee <buggyc.qz6o3@silomails.com>
2023-03-24 03:22:10 +0200

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,18 +11,16 @@ int main(void)
 	int start;
 
 	for (start = 1; start <= 100; start++)
-	{
 		if (start % 3 == 0 && start % 5 == 0)
 			printf("FizzBuzz ");
 		else if (start % 3 == 0)
 			printf("Fizz ");
 		else if (start % 5 == 0)
 			printf("Buzz ");
-		else if (start == 100)
-			printf("%d\n", start);
 		else
 			printf("%d ", start);
-	}
+
+	putchar('\n');
 
 	return (0);
 }
