2a73a18f361c4133e33a81f2c0a9bc68283d4177
buggyCodee <buggyc.qz6o3@silomails.com>
2023-03-25 02:30:08 +0200

    let us try recursion

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,49 +1,6 @@
 #include "main.h"
 
 /**
- * count_digits - function that count the number of digits to any given integer
- *
- * @num: number to calculate its digit
- *
- * Return: digits of the number
- */
-
-int count_digits(int num)
-{
-	int digits = 0;
-
-	while (num != 0)
-	{
-		num /= 10;
-		digits++;
-	}
-
-	if (digits < 1)
-		digits++;
-
-	return (digits);
-}
-
-/**
- * power - calcualte the power of the given input
- *
- * @base: base
- * @exp: exponent
- *
- * Return: base ^ exponent
- */
-
-int power(int base, int exp)
-{
-	int res = 1;
-
-	while (exp--)
-		res *= base;
-
-	return (res);
-}
-
-/**
  * print_number - function that prints an integer
  *
  * @n: number to be printed
@@ -53,14 +10,14 @@ int power(int base, int exp)
 
 void print_number(int n)
 {
-	int digits = count_digits(n);
-
 	if (n < 0)
 	{
 		n = -n;
 		_putchar('-');
 	}
 
-	while (digits--)
-		_putchar('0' + (n / power(10, digits)) % 10);
+	if (n / 10 > 0)
+		print_number(n/10);
+
+	_putchar('0' + (n % 10));
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,6 +11,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int count_digits(int num);
-int power(int base, int exp);
 void print_number(int n);
