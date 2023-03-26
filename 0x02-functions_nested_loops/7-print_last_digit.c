0288a55c37cf138aa59da8c520d2090e767c114d
buggyCodee <buggyc.qz6o3@silomails.com>
2023-03-27 00:00:48 +0200

    fixing boundary test INT_MIN (overflow)

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * print_last_digit - function that prints the last digit of a number
@@ -10,6 +11,12 @@
 
 int print_last_digit(int n)
 {
+	if (n == INT_MIN)
+	{
+		_putchar('8');
+		return (8);
+	}
+
 	if (n < 0)
 		n *= -1;
 
