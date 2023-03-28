4de77b967ec4e7b70b4a215e6613262436400a5f
buggyCodee <buggyc.qz6o3@silomails.com>
2023-03-28 20:08:43 +0200

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: string to get it's size
+ *
+ * Return: int that's the size of the string
+ */
+
+int _strlen(char *s)
+{
+	int size = 0;
+
+	while (s[size++] != '\0')
+
+	return (size);
+}
