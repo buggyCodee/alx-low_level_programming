dc2854b50d171ce86d30bd03bde041326ee0f6d3
buggyCodee <buggyc.qz6o3@silomails.com>
2023-03-30 03:02:51 +0200

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings
+ *
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ *
+ * Return:	0, if the s1 and s2 are equal;
+ *		negative value if s1 is less than s2;
+ *		a positive value if s1 is greater than s2.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int value = 0;
+	int s1_size = 0;
+	int s2_size = 0;
+	int i;
+
+	while (s1[s1_size] != '\0')
+		s1_size++;
+
+	while (s2[s2_size] != '\0')
+		s2_size++;
+
+	int min_size = s1_size;
+
+	if (s2_size < s1_size)
+		min_size = s2_size;
+
+	for (i = 0; i < min_size; i++)
+		if (s1[i] > s2[i])
+		{
+			value = 1;
+			break;
+		}
+		else
+		{
+			value = -1;
+			break;
+		}
+
+	if (value == 0 && s1_size != s2_size)
+	{
+		if (s1_size < s2_size)
+			value = 1;
+		else
+			value = -1;
+	}
+
+	return (value);
+}
