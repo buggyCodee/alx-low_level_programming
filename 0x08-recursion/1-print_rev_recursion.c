c4ab0125016b7f1ffdaba6535d1305e93fbfb470
buggyCodee <buggyc.qz6o3@silomails.com>
2023-04-05 15:50:59 +0200

    Why is it so important to dream? Because, in my dreams we are together

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _puts_rev - puts te character in a reverse order from specified index
+ *
+ * @s: string to be printed
+ * @i: current index in the string
+ *
+ * Return: void
+ */
+
+void _puts_rev(char *s, int i)
+{
+	if (s[i] == '\0')
+		return;
+	_puts_rev(s, i + 1);
+	_putchar(s[i]);
+}
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ *
+ * @s: string to be printed in reverse
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	_puts_rev(s, 0);
+	_putchar('\n');
+}
