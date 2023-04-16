6bb5b8e6fc20fe44fab0a71ff18f004ebd75d38f
buggyCodee <buggyc.qz6o3@silomails.com>
2023-04-17 00:21:06 +0200

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point of the program that prints the sentece
+ *		with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
