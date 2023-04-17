a83a8aab59c643b28466cc1fe943d8b29f78fe7f
buggyCodee <buggyc.qz6o3@silomails.com>
2023-04-17 16:16:55 +0200

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef ALX_0X0E_DOG_H_
+#define ALX_0X0E_DOG_H_
+
+/**
+ * struct dog - structure to the dog
+ *
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
