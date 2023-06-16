260adb21044baf3f8fadc46de5a282e0e9ca6422
buggyCodee <buggyc.qz6o3@silomails.com>
2023-06-16 23:55:53 +0300

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -42,8 +42,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int size_two = length(s2);
 	unsigned int total_size = 0;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
 
 	if (n > size_two)
 		n = size_two;
