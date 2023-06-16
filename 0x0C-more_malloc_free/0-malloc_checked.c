c5b3b2d88324a37897686aae73a94ed8e36e761f
buggyCodee <buggyc.qz6o3@silomails.com>
2023-06-16 23:09:58 +0300

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ *
+ * @b: the number of bytes to be allocated
+ *
+ * Return:	pointer to the allocated memory
+ *		if malloc fails, the malloc_checked function should cause
+ *			normal process termination with a status value of 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *new_memory = malloc(b);
+
+	if (new_memory == NULL)
+		exit(98);
+
+	return (new_memory);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef ALX_0X0C_MORE_MALLOC_FREE_MAIN_H_
+#deinfe ALX_0X0C_MORE_MALLOC_FREE_MAIN_H_
+
+void *malloc_checked(unsigned int b);
+
+#endif
