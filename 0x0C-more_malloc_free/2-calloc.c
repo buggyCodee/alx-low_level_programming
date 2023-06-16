367b67c3d1dbde34284e43b228d97f49af9395ac
buggyCodee <buggyc.qz6o3@silomails.com>
2023-06-17 01:01:30 +0300

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb <= 0 || size <= 0 || new_memory == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb * size; i++)
+	for (i = 0; i < size; i++)
 		new_memory[i] = 0;
 
 	return (new_memory);
