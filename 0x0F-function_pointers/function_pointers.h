c0765f2e0095d40ab27d42da24af67366ac09570
buggyCodee <buggyc.qz6o3@silomails.com>
2023-06-21 22:52:13 +0300

    1. If you spend too much time thinking about a thing, you will never get it done

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* ALX_0x0F_FUNCTION_POINTERS_MAIN_H_ */
