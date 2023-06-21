fcf4d8df06c735c79a8a3822c8ec97eef09b3534
buggyCodee <buggyc.qz6o3@silomails.com>
2023-06-21 22:35:57 +0300

    0. What is my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+/**
+ * print_name - function that prints a name
+ *
+ * @name: the name to be printed
+ * @f: function pointer to anoter function
+ *
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef ALX_0x0F_FUNCTION_POINTERS_MAIN_H_
+#define ALX_0x0F_FUNCTION_POINTERS_MAIN_H_
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif /* ALX_0x0F_FUNCTION_POINTERS_MAIN_H_ */
