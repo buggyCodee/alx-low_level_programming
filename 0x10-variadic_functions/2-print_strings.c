c309917820ed69cd5345f6fcb95bf5c608607fa9
buggyCodee <buggyc.qz6o3@silomails.com>
2023-06-23 02:54:46 +0300

    2. One woman is constant is another woman is variable

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function that prints strings, followed by a new line
+ *
+ * @separator: the string to be printed between the strings
+ * @n: the number of strings passed to the function
+ *
+ * Return: Nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i && separator != NULL)
+			printf("%s", separator);
+		printf("%s", va_arg(args, char *));
+	}
+	printf("\n");
+
+	va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,6 @@
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif /* ALX_0X10_VARIADIC_FUNCTIONS_H_ */
