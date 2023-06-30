1c0182bb1a3c1da47aff574444c40d7e220c300e
buggyCodee <buggyc.qz6o3@silomails.com>
2023-06-30 23:08:57 +0300

    3. Add node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,13 +12,14 @@
  * Return: address of the new element, or NULL if it failed
  */
 
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (new_node);
 
+	new_node->next = NULL;
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 
