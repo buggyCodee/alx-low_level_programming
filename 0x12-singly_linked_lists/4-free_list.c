f9dfef744cc3189aa7d682db6e67aaa8cad0a570
buggyCodee <buggyc.qz6o3@silomails.com>
2023-06-30 23:50:54 +0300

    4. Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ *
+ * @head: pointer to the first node to the list to be freed
+ *
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+	while (head)
+	{
+		list_t *temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,5 +21,6 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif /* ALX_0X12_SINGLY_LINKED_LISTS_H_ */
