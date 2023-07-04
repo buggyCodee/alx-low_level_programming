af303e4badda4c644247ea3dab9119a5af546bcc
buggyCodee <buggyc.qz6o3@silomails.com>
2023-07-04 22:06:25 +0300

    2. Add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,11 +14,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *node = malloc(sizeof(listint_t));
 
-	if (node != NULL)
-	{
-		node->n = n;
-		node->next = *head;
-	}
+	if (node == NULL)
+		return (node);
+
+	node->n = n;
+	node->next = *head;
+	*head = node;
 
 	return (node);
 }
