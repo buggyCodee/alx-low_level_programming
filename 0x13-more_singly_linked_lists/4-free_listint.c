cdce9600ac7fb1f21766d99814b0ffd3875eeeec
buggyCodee <buggyc.qz6o3@silomails.com>
2023-07-04 23:46:59 +0300

    4. Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a listint_t list
+ *
+ * @head: first node of the list
+ *
+ * Return: Nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = temp->next;
+
+		free(head);
+		head = temp;
+	}
+}
