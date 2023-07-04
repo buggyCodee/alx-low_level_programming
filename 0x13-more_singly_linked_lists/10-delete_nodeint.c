82b37db5cf597d694d3a2853c86a6ef24293597b
buggyCodee <buggyc.qz6o3@silomails.com>
2023-07-05 01:43:54 +0300

    10. Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 
 	if (prev == NULL)
-		prev = current->next;
+		*head = current->next;
 	else
 		prev->next = current->next;
 
